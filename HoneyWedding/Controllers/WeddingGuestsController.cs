238ab99f9fa11aa131a9847c4f76976ea4587778
russell-cook <russ.cook@gmail.com>
2016-07-21 23:43:58 -0700

    Add AdminRsvpNotification email

--- HoneyWedding/Controllers/WeddingGuestsController.cs
+++ HoneyWedding/Controllers/WeddingGuestsController.cs
@@ -10,6 +10,8 @@ using Omu.ValueInjecter;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Configuration;
 
 namespace HoneyWedding.Controllers
 {
@@ -183,6 +185,8 @@ namespace HoneyWedding.Controllers
             if (ModelState.IsValid)
             {
                 var guest = await _unitOfWork.WeddingGuestRepository.GetByIDAsync(model.Id);
+                var oldResponse = new WeddingGuest();
+                oldResponse.InjectFrom(guest);
                 guest.InjectFrom(model);
                 if (!guest.DidRsvp)
                 {
@@ -197,91 +201,14 @@ namespace HoneyWedding.Controllers
                 }
                 await _unitOfWork.SaveAsync();
 
-            // send confirmation email
-                string body;
-                string greeting = guest.UpdatedRsvp ? "We received your updated RSVP info. " : "Thank you for your RSVP. ";
-                if (guest.CanAttend == true)
-                {
-                    greeting += "We're so glad you'll be able to join us";
-                    if (guest.PlusOneCanAtend == false && guest.PlusOneIsKnown)
-                    {
-                        greeting += string.Format(", but we're sorry {0} won't be able to make it.", guest.FirstNamePlusOne);
-                    }
-                    else
-                    {
-                        greeting += ".";
-                    }
-                }
-                else
-                {
-                    greeting += "We're sorry you won't be able to join us.";
-                }
+                // send RSVP Confirmation email
+                await SendRsvpEmailAsync(guest);
 
-                // format info table
-                var attendMessage = string.Empty;
-                switch (guest.CanAttend)
+                // send Admin notification email
+                if ((guest.UpdatedRsvp && guest.CanAttend == false) || (guest.UpdatedRsvp && guest.HasPlusOne && oldResponse.PlusOneCanAtend == true && guest.PlusOneCanAtend == false))
                 {
-                    case true:
-                        attendMessage = "Yes";
-                        break;
-                    case false:
-                        attendMessage = "No";
-                        break;
-                    case null:
-                        attendMessage = "Unsure";
-                        break;
-                    default:
-                        break;
+                    await SendAdminRsvpNotificationAsync(guest, oldResponse);
                 }
-                string infoTable = string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Can you attend?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", attendMessage);
-                if (guest.CanAttend == true)
-                {
-                    infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would you like a meatless option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", guest.Meatless == true ? "Yes" : "No");
-                    infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would you like a vegan option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", guest.Vegan == true ? "Yes" : "No");
-                    infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Dietary needs:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", guest.DietaryNotes);
-                    if (guest.HasPlusOne)
-                    {
-                        var PlusOneAttendMessage = string.Empty;
-                        switch (guest.PlusOneCanAtend)
-                        {
-                            case true:
-                                PlusOneAttendMessage = "Yes";
-                                break;
-                            case false:
-                                PlusOneAttendMessage = "No";
-                                break;
-                            case null:
-                                PlusOneAttendMessage = "Unsure";
-                                break;
-                            default:
-                                break;
-                        }
-
-                        infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >{0}:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", guest.PlusOneIsKnown ? string.Format("Will {0} be able to attend?", guest.FirstNamePlusOne) : "Would you like to bring a 'plus one'?", PlusOneAttendMessage);
-                        if (!guest.PlusOneIsKnown == true && guest.PlusOneCanAtend == true)
-                        {
-                            infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Name of your 'plus one':</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", guest.FirstNamePlusOne + " " + guest.LastNamePlusOne);
-                        }
-                        if (guest.PlusOneCanAtend == true)
-                        {
-                            infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would {0} like a meatless option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", guest.PlusOneIsKnown ? guest.FirstNamePlusOne : "your 'plus one'", guest.Meatless == true ? "Yes" : "No");
-                            infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would {0} like a vegan option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", guest.PlusOneIsKnown ? guest.FirstNamePlusOne : "your 'plus one'", guest.Vegan == true ? "Yes" : "No");
-                            infoTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Dietary needs for {0}:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", guest.PlusOneIsKnown ? guest.FirstNamePlusOne : "your 'plus one'", guest.DietaryNotes);
-                        }
-                    }
-                }
-
-                string instructions = guest.CanAttend != false ? "If this information is incorrect, or if you need to change your responses in the future, please use the following link:" : "If your plans change and you'd like to attend, please email Lisa and Russ as soon as possible at <a href = \"mailto:contact@thehoneyswedding.net?subject=Please Change My RSVP\">contact@thehoneyswedding.com</a>, and we'll do our best to accommodate you";
-                var callbackUrl = Url.Action("RSVP", "WeddingGuests", new { userId = guest.Id }, protocol: Request.Url.Scheme);
-                var button = guest.CanAttend != false ? string.Format("<table border='0' cellpadding='0' cellspacing='12' width='100%'><tr><td align='center'><table border='0' cellpadding='0' cellspacing='0'><tr><td bgcolor='#f46e6c' style='padding: 12px 18px 12px 18px; -webkit-border-radius: 3px; border-radius: 3px; background-color: #ff5858;' align='center'><a href='{0}' style='text-decoration: none; font-size: 18px; color: #ffffff; '>Update RSVP Info</a></td></tr></table></td></tr></table>", callbackUrl) : "";
-
-                using (var sr = new StreamReader(Server.MapPath("\\Templates\\") + "RsvpConfirmationEmailTemplate.html"))
-                {
-                    body = await sr.ReadToEndAsync();
-                }
-                string messageBody = string.Format(body, greeting, guest.FirstName, infoTable, instructions, button);
-                string subject = guest.UpdatedRsvp ? "Updated RSVP Confirmation--Russ and Lisa's Wedding" : "RSVP Confirmation--Russ and Lisa's Wedding";
-                await UserManager.SendEmailAsync(guest.Id, subject, messageBody);
 
                 return RedirectToAction("RSVPConfirmation", new { userId = model.Id });
             }
@@ -332,6 +259,157 @@ namespace HoneyWedding.Controllers
 
             return View(viewModel);
         }
+
+        public async Task SendAdminRsvpNotificationAsync(WeddingGuest guest, WeddingGuest oldResponse)
+        {
+            string body;
+            string greeting = guest.UpdatedRsvp ? "Updated RSVP Notification: " : "RSVP Notification: ";
+            greeting += string.Format("{0} {1}", guest.FirstName, guest.LastName);
+
+            string oldRsvpHeader = "";
+            string oldRsvpTable = "";
+            string newRsvpHeader = "";
+            string newRsvpTable = "";
+
+            if (guest.UpdatedRsvp)
+            {
+                oldRsvpHeader = "<h4>Old RSVP Responses:</h4>";
+                newRsvpHeader = "<h4>New RSVP Responses:</h4>";
+                oldRsvpTable = FormatRsvpTable(oldResponse);
+            }
+            else
+            {
+                newRsvpHeader = "<h4>RSVP Responses:</h4>";
+            }
+
+            newRsvpTable = FormatRsvpTable(guest);
+
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("contact@thehoneyswedding.net")); 
+            message.From = new MailAddress("noreply@thehoneyswedding.net");
+            message.Subject = greeting;
+            using (var sr = new StreamReader(Server.MapPath("\\Templates\\") + "AdminRsvpNotificationEmailTemplate.html"))
+            {
+                body = await sr.ReadToEndAsync();
+            }
+            message.Body = string.Format(body, greeting, oldRsvpHeader, oldRsvpTable, newRsvpHeader, newRsvpTable);
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = ConfigurationManager.AppSettings["MAIL_USER"],
+                    Password = ConfigurationManager.AppSettings["MAIL_PASSWORD"]  
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.sendgrid.net";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+            }
+        }
+
+        public async Task SendRsvpEmailAsync(WeddingGuest guest)
+        {
+            string body;
+            string greeting = guest.UpdatedRsvp ? "We received your updated RSVP info. " : "Thank you for your RSVP. ";
+            if (guest.CanAttend == true)
+            {
+                greeting += "We're so glad you'll be able to join us";
+                if (guest.PlusOneCanAtend == false && guest.PlusOneIsKnown)
+                {
+                    greeting += string.Format(", but we're sorry {0} won't be able to make it.", guest.FirstNamePlusOne);
+                }
+                else
+                {
+                    greeting += ".";
+                }
+            }
+            else
+            {
+                greeting += "We're sorry you won't be able to join us.";
+            }
+
+            // format info table
+            string infoTable = FormatRsvpTable(guest);
+            //string infoTable = "";
+
+            string instructions = guest.CanAttend != false ? "If this information is incorrect, or if you need to change your responses in the future, please use the following link:" : "If your plans change and you'd like to attend, please email Lisa and Russ as soon as possible at <a href = \"mailto:contact@thehoneyswedding.net?subject=Please Change My RSVP\">contact@thehoneyswedding.com</a>, and we'll do our best to accommodate you";
+            var callbackUrl = Url.Action("RSVP", "WeddingGuests", new { userId = guest.Id }, protocol: Request.Url.Scheme);
+            var button = guest.CanAttend != false ? string.Format("<table border='0' cellpadding='0' cellspacing='12' width='100%'><tr><td align='center'><table border='0' cellpadding='0' cellspacing='0'><tr><td bgcolor='#853641' style='padding: 12px 18px 12px 18px; -webkit-border-radius: 3px; border-radius: 3px; background-color: #853641;' align='center'><a href='{0}' style='text-decoration: none; font-size: 18px; color: #ffffff; '>Update RSVP Info</a></td></tr></table></td></tr></table>", callbackUrl) : "";
+
+            using (var sr = new StreamReader(Server.MapPath("\\Templates\\") + "RsvpConfirmationEmailTemplate.html"))
+            {
+                body = await sr.ReadToEndAsync();
+            }
+            string messageBody = string.Format(body, greeting, guest.FirstName, infoTable, instructions, button);
+            string subject = guest.UpdatedRsvp ? "Updated RSVP Confirmation--Russ and Lisa's Wedding" : "RSVP Confirmation--Russ and Lisa's Wedding";
+            await UserManager.SendEmailAsync(guest.Id, subject, messageBody);
+        }
+
+        public string FormatRsvpTable(WeddingGuest model)
+        {
+            string rsvpTable = "<table border='0' cellpadding='6' cellspacing='0' width='100%'>";
+
+            var attendMessage = string.Empty;
+            switch (model.CanAttend)
+            {
+                case true:
+                    attendMessage = "Yes";
+                    break;
+                case false:
+                    attendMessage = "No";
+                    break;
+                case null:
+                    attendMessage = "Unsure";
+                    break;
+                default:
+                    break;
+            }
+            rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Can you attend?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", attendMessage);
+
+            if (model.CanAttend == true)
+            {
+                rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would you like a meatless option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", model.Meatless == true ? "Yes" : "No");
+                rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would you like a vegan option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", model.Vegan == true ? "Yes" : "No");
+                rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Dietary needs:</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", model.DietaryNotes);
+                if (model.HasPlusOne)
+                {
+                    var PlusOneAttendMessage = string.Empty;
+                    switch (model.PlusOneCanAtend)
+                    {
+                        case true:
+                            PlusOneAttendMessage = "Yes";
+                            break;
+                        case false:
+                            PlusOneAttendMessage = "No";
+                            break;
+                        case null:
+                            PlusOneAttendMessage = "Unsure";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >{0}:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", model.PlusOneIsKnown ? string.Format("Will {0} be able to attend?", model.FirstNamePlusOne) : "Would you like to bring a 'plus one'?", PlusOneAttendMessage);
+                    if (!model.PlusOneIsKnown == true && model.PlusOneCanAtend == true)
+                    {
+                        rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Name of your 'plus one':</ td ><td width = '75%' align = 'left' valign = 'top' >{0}</ td ></ tr >", model.FirstNamePlusOne + " " + model.LastNamePlusOne);
+                    }
+                    if (model.PlusOneCanAtend == true)
+                    {
+                        rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would {0} like a meatless option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", model.PlusOneIsKnown ? model.FirstNamePlusOne : "your 'plus one'", model.Meatless == true ? "Yes" : "No");
+                        rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Would {0} like a vegan option?:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", model.PlusOneIsKnown ? model.FirstNamePlusOne : "your 'plus one'", model.Vegan == true ? "Yes" : "No");
+                        rsvpTable += string.Format("<tr><td width = '25%' align = 'right' valign = 'top' >Dietary needs for {0}:</ td ><td width = '75%' align = 'left' valign = 'top' >{1}</ td ></ tr >", model.PlusOneIsKnown ? model.FirstNamePlusOne : "your 'plus one'", model.DietaryNotes);
+                    }
+                }
+            }
+            rsvpTable += "</table>";
+            return rsvpTable;
+        }
+
+
 
     }
 }
